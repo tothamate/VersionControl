48fff53c2c3d17910c3785abdb0f3dfb60c96ae2
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-03-14 08:49:00 +0100

    Excel munkafüzet létrehozása és megnyitása

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -19,6 +19,7 @@ namespace gyak4
         public Form1()
         {
             InitializeComponent();
+            CreateExcel();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,5 +30,43 @@ namespace gyak4
         {
             Flats = context.Flat.ToList();
         }
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+            
     }
 }
