075d34e2de7cc8b42723d61da2b53c4fa3989ae5
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-03-14 09:29:06 +0100

    kimenet formazasanak fele

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -107,6 +107,16 @@ namespace gyak4
             GetCell(2, 1),
             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+
         }
 
         private string GetCell(int x, int y)
