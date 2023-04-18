9e59c43ae008e5c0f96ddc405bfb6b8c45293a63
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-04-18 08:49:39 +0200

    Terméktörzs összeállítása

--- mintazh/mintazh/Form1.cs
+++ mintazh/mintazh/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using mintazh.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,31 @@ namespace mintazh
 {
     public partial class Form1 : Form
     {
+        private List<Product> _products = new List<Product>();
+
         public Form1()
         {
             InitializeComponent();
+            var valami = LoadXml("Menu.xml");
         }
+
+        public string LoadXml(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+            {
+                //var output = sr.ReadLine();
+                //while (!sr.EndOfStream)
+                //{
+                //    output += "\n" + sr.ReadLine();
+                //}
+                //return output;
+
+                return sr.ReadToEnd();
+            }
+
+            
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
