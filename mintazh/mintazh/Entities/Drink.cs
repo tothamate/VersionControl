ed5448936417c466020d40b0b3247922a152e630
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-04-18 08:34:56 +0200

    Product implementálása

---
+++ mintazh/mintazh/Entities/Drink.cs
@@ -0,0 +1,18 @@
+﻿using mintazh.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mintazh.Entities
+{
+    public class Drink : Product
+    {
+        public override void Display()
+        {
+            BackColor = Color.LightBlue;
+        }
+    }
+}
---
+++ mintazh/mintazh/Entities/Food.cs
@@ -0,0 +1,47 @@
+﻿using mintazh.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mintazh.Entities
+{
+    class Food : Product
+    {
+        public string Description { get; set; }
+
+        public Food()
+        {
+            Click += Food_Click;
+        }
+
+        private void Food_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                string.Format("Cím: {0}\nMegjegyzés: {1}",
+                Title,
+                Description));
+        }
+
+        public override void Display()
+        {
+            if (Calories < 750)
+            {
+                BackColor = Color.LightGreen;
+            }
+
+            else if (Calories <= 1000)
+            {
+                BackColor = Color.LightYellow;
+            }
+
+            else
+            {
+                BackColor = Color.Salmon;
+            }
+        }
+    }
+}
