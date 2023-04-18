8ba3b053c34db82d32a257d596d116d6efbe7001
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-04-18 08:19:01 +0200

    Product

---
+++ mintazh/mintazh/Abstractions/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mintazh.Abstractions
+{
+    public abstract class Product : Button
+    {
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set 
+            { 
+                _title = value;
+                Text = _title;
+            }
+        }
+
+        private int _calories;
+
+        public int Calories
+        {
+            get { return _calories; }
+            set 
+            {
+                _calories = value;
+                Display();
+            }
+        }
+
+        public Product()
+        {
+            Width = 150;
+            Height = 50;
+        }
+
+        public abstract void Display();
+    }
+}
