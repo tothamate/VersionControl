b01d55b93fae3908b938afe2e7a49e304f4a0fbc
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-04-18 09:10:06 +0200

    XML beolvasása

--- mintazh/mintazh/Form1.cs
+++ mintazh/mintazh/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using mintazh.Abstractions;
+using mintazh.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace mintazh
 {
@@ -19,7 +21,7 @@ namespace mintazh
         public Form1()
         {
             InitializeComponent();
-            var valami = LoadXml("Menu.xml");
+            
         }
 
         public string LoadXml(string fileName)
@@ -39,6 +41,41 @@ namespace mintazh
             
         }
 
+        public void ProcessXml()
+        {
+            var xmlText = LoadXml("Menu.xml");
+            var xml = new XmlDocument();
+            xml.LoadXml(xmlText);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                var name = item.SelectSingleNode("name").InnerText;
+                var description = item.SelectSingleNode("description").InnerText;
+                var calories = item.SelectSingleNode("calories").InnerText;
+                var type = item.SelectSingleNode("type").InnerText;
+
+                if(type.Equals("food"))
+                {
+                    var food = new Food()
+                    {
+                        Title = name,
+                        Description = description,
+                        Calories = int.Parse(calories)
+                    };
+                _products.Add(food);
+                }
+
+                if (type.Equals("drink"))
+                {
+                    var drink = new Drink()
+                    {
+                        Title = name,
+                        Calories = int.Parse(calories)
+                    };
+                    _products.Add(drink);
+                }
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
