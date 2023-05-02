58cc3ace3ae0718a4d909db9d56392876ad9fb8c
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-02 09:02:13 +0200

    jelszó validálás teszt

--- gyak8/UnitTestProject1.Test/AccountControllerTestFixture.cs
+++ gyak8/UnitTestProject1.Test/AccountControllerTestFixture.cs
@@ -10,7 +10,13 @@ namespace UnitTestProject1.Test
 {
     class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -18,6 +24,26 @@ namespace UnitTestProject1.Test
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("ABCDabcd", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("A1", false),
+            TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
