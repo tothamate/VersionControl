6d4877f369496bf6ebf181bc09a7ab545c5ae9c5
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-02 09:23:28 +0200

    Register függvény Validálás kivétel

--- gyak8/UnitTestProject1.Test/AccountControllerTestFixture.cs
+++ gyak8/UnitTestProject1.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -66,6 +67,33 @@ namespace UnitTestProject1.Test
             Assert.AreEqual(email, actualResult.Email);
             Assert.AreEqual(password, actualResult.Password);
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus", "Abcd1234"),
+            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
         }
     }
 }
