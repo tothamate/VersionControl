3b21d92efc0fc5a747d70b20862aa87ae4f8b4d5
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-02 08:39:56 +0200

    E-mail validálás teszt függvény

---
+++ gyak8/UnitTestProject1.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            
+        }
+    }
+}
