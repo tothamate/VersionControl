4380e0881937b1240bc5c85f2b168aff673b8eac
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-09 08:33:54 +0200

    Osztályok létrehozva

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -15,6 +15,12 @@ namespace week09
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
