6fe3cdcb356c4ba3c6bcc9ec17f0deb6687b1f86
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-09 09:19:01 +0200

    szimuláció

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -14,6 +14,7 @@ namespace week09
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
@@ -24,7 +25,58 @@ namespace week09
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                    SimStep(year, Population[i]);
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} \t Fiúk:{1} \t Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
+
+            private void SimStep(int year, Person person)
+            {
+                //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+                if (!person.IsAlive) return;
+
+                // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+                byte age = (byte)(year - person.BirthYear);
+
+                // Halál kezelése
+                // Halálozási valószínűség kikeresése
+                double pDeath = (from x in DeathProbabilities
+                                 where x.Gender == person.Gender && x.Age == age
+                                 select x.P).FirstOrDefault();
+                // Meghal a személy?
+                if (rng.NextDouble() <= pDeath)
+                    person.IsAlive = false;
+
+                //Születés kezelése - csak az élő nők szülnek
+                if (person.IsAlive && person.Gender == Gender.Female)
+                {
+                    //Szülési valószínűség kikeresése
+                    double pBirth = (from x in BirthProbabilities
+                                     where x.Age == age
+                                     select x.P).FirstOrDefault();
+                    //Születik gyermek?
+                    if (rng.NextDouble() <= pBirth)
+                    {
+                        Person újszülött = new Person();
+                        újszülött.BirthYear = year;
+                        újszülött.NbrOfChildren = 0;
+                        újszülött.Gender = (Gender)(rng.Next(1, 3));
+                        Population.Add(újszülött);
+                    }
+                }
+            }
 
         private List<DeathProbability> GetDeathProbabilities(string csvpath)
         {
