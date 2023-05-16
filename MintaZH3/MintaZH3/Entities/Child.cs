f2b08c24acee1c4f5ba62e66ff6c33aa9b1cfc94
o3vcg7 <mate.toth9@stud.uni-corvinus.hu>
2023-05-16 08:20:52 +0200

    start

---
+++ MintaZH3/MintaZH3/Entities/Child.cs
@@ -0,0 +1,15 @@
+﻿using MintaZH3.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH3.Entities
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public Behaviour YearlyBehaviour { get; set; }
+    }
+}
---
+++ MintaZH3/MintaZH3/Enum/Behaviour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH3.Enum
+{
+    public enum Behaviour
+    {
+        Worst = 1,
+        Bad = 2,
+        Good = 3,
+        Better = 4,
+        Best = 5
+    }
+}
